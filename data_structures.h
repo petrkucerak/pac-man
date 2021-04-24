78300b6ac5cff3db12d32c38b308e9e05b3bdb6b
Lukas Nejezchleb <nejezluk@fel.cvut.cz>
2021-04-24 09:37:29 +0200

    Added basic functions to controll peripherals

---
+++ data_structures.h
@@ -0,0 +1,50 @@
+/*******************************************************************
+  Data structures definitions for project apoman for MicroZed based MZ_APO board
+  designed by Petr Porazil at PiKRON
+
+  data_structures.c      - file with definitions of used data structures
+
+(C) Copyright 2021 by Petr Kucera and Lukas Nejezchleb
+      e-mails:   kucerp28@fel.cvut.cz , nejezluk@fel.cvut.cz
+      license:  any combination of GPL, LGPL, MPL or BSD licenses
+ *******************************************************************/
+
+#ifndef DATA_STRUCTURES_H
+#define DATA_STRUCTURES_H
+#include <stdint.h>
+
+typedef struct{
+    int width;
+    int height;
+    uint16_t *fb;
+}fb_data;
+
+typedef struct{
+    int width;
+    int height;
+    char *board_arr;
+    int max_object_diameter; //maximum dimensions of ghosts, pacman and coins
+}map_data;
+
+typedef struct{
+    int x;
+    int y;
+}coords;
+
+
+//place where is the coin drawn, pacman can walk accross it
+#define COIN 3
+
+//black place on the map, pacman is allowed to walk there
+#define PASSAGE 1 
+
+//place that will be drawn in color to show where the wall is
+#define WALL    2
+
+//black place on the map, pacman is not allowed there
+#define BLOCKED 0 
+
+//place where is the supercoin drawn, pacman can walk accross it
+#define SUPERCOIN 4
+
+#endif
---
+++ update_peripherals.h
@@ -0,0 +1,29 @@
+/*******************************************************************
+  Simple program to update peripherals on MicroZed
+  based MZ_APO board designed by Petr Porazil at PiKRON
+
+  update_peripherals.c      - simple program to update peripherals
+
+  (C) Copyright 2017 by Lukas Nejezchleb
+      e-mail:   nejezluk@fel.cvut.cz
+      license:  any combination of GPL, LGPL, MPL or BSD licenses
+
+ *******************************************************************/
+
+#ifndef UPDATE_LCD_H
+#define UPDATE_LCD_H
+#include <stdint.h>
+#include "data_structures.h"
+
+//function that updates the LCD from frame buffer. LCD has to be initialised
+void lcd_from_fb(const fb_data* frame_buff, unsigned char *parlcd_mem_base);
+
+//function that shows nr on the ledstrip
+void led_strip_number(unsigned char *led_mem_base, int max_nr, int nr);
+
+//sets left RGB to particular color
+void set_left_RGB(unsigned char *led_mem_base, uint8_t r, uint8_t g, uint8_t b);
+
+//sets right RGB to particular color
+void set_right_RGB(unsigned char *led_mem_base, uint8_t r, uint8_t g, uint8_t b);
+#endif
