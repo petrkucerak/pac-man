5a77144ac2868edda498f240364d74d30d2460cd
Lukas Nejezchleb <nejezluk@fel.cvut.cz>
2021-04-24 10:09:32 +0200

    Added first map

---
+++ map_circles.c
@@ -0,0 +1,42 @@
+/*******************************************************************
+  Map circles for apoman on MicroZed
+  based MZ_APO board designed by Petr Porazil at PiKRON
+
+  map_circles.c      - first map
+
+  (C) Copyright 2021 by Lukas Nejezchleb
+      e-mail:   nejezluk@fel.cvut.cz
+      license:  any combination of GPL, LGPL, MPL or BSD licenses
+
+ *******************************************************************/
+#include "map_template.h"
+
+static char board_data[] = {
+1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+1,3,2,2,2,2,2,2,2,2,2,2,2,3,1,
+1,2,1,1,1,1,1,1,1,1,1,1,1,2,1,
+1,2,1,2,2,2,2,2,2,2,2,2,1,2,1,
+1,2,1,2,1,1,1,1,1,1,1,2,1,2,1,
+1,2,2,2,2,2,2,2,2,2,2,2,2,2,1,
+1,2,1,2,1,1,1,1,1,1,1,2,1,2,1,
+1,2,1,2,2,2,2,2,2,2,2,2,1,2,1,
+1,2,1,1,1,1,1,1,1,1,1,1,1,2,1,
+1,3,2,2,2,2,2,2,2,2,2,2,2,3,1,
+1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+};
+
+
+map_template map_apo_of_doom = {
+    .name = "Concentric_circles",
+    .pacman_spawn_x = 1, /*spawning location of pacman*/
+    .pacman_spawn_y = 9, /*spawning location of pacman*/
+    .ghost_spawn_x = 7,  /*spawning location of ghost*/
+    .ghost_spawn_y = 3,  /*spawning location of ghost*/
+    .width = 15,         /*width of the map*/
+    .height = 11,        /*height of the map*/
+    .board = board_data, /*actual data about the content of map*/
+    .coin = 2,           /*value used to describe coin*/
+    .wall = 1,           /*value used to describe wall*/
+    .special = 3,        /*value used to describe special coin*/
+    .blank = 0,          /*value used to describe place without coin*/
+};
---
+++ map_template.h
@@ -0,0 +1,35 @@
+/*******************************************************************
+  Maps for apoman on MicroZed
+  based MZ_APO board designed by Petr Porazil at PiKRON
+
+  map_template      - place with recepie for maps as well as all maps
+
+  (C) Copyright 2021 by Lukas Nejezchleb
+      e-mail:   nejezluk@fel.cvut.cz
+      license:  any combination of GPL, LGPL, MPL or BSD licenses
+
+ *******************************************************************/
+
+#ifndef MAP_TEMPLATE_H
+#define MAP_TEMPLATE_H
+
+typedef struct
+{
+    char *name;         /* name of the map*/
+    int pacman_spawn_x; /*spawning location of pacman*/
+    int pacman_spawn_y; /*spawning location of pacman*/
+    int ghost_spawn_x;  /*spawning location of ghost*/
+    int ghost_spawn_y;  /*spawning location of ghost*/
+    int width;          /*width of the map*/
+    int height;         /*height of the map*/
+    char *board;        /*actual data about the content of map*/
+    const int coin;          /*value used to describe coin*/
+    const int wall;          /*value used to describe wall*/
+    const int special;       /*value used to describe special coin*/
+    const int blank;          /*value used to describe place without coin*/
+} map_template;
+
+//first map
+extern map_template map_circles;
+
+#endif
