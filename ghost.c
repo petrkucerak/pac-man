8846707935593c2a30be5d0dc8aec24d7b3697d9
Lukas Nejezchleb <nejezluk@fel.cvut.cz>
2021-04-28 19:28:02 +0200

    Created ghost file

---
+++ ghost.c
@@ -0,0 +1,12 @@
+/*******************************************************************
+  All inportant functions for ghost on MicroZed
+  based MZ_APO board designed by Petr Porazil at PiKRON
+
+  ghost.c      - all important functions for ghost
+
+  (C) Copyright 2021 by Lukas Nejezchleb
+      e-mail:   nejezluk@fel.cvut.cz
+      license:  any combination of GPL, LGPL, MPL or BSD licenses
+
+ *******************************************************************/
+#include "ghost.h"
---
+++ ghost.h
@@ -0,0 +1,15 @@
+/*******************************************************************
+  All inportant functions for ghost on MicroZed
+  based MZ_APO board designed by Petr Porazil at PiKRON
+
+  ghost.h      - all important functions for ghost
+
+  (C) Copyright 2021 by Lukas Nejezchleb
+      e-mail:   nejezluk@fel.cvut.cz
+      license:  any combination of GPL, LGPL, MPL or BSD licenses
+
+ *******************************************************************/
+#ifndef GHOST_H
+#define GHOST_H
+
+#endif
