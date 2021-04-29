28bccbec96282b631ac09a69dea1ec1c6835b4a2
Lukas Nejezchleb <nejezluk@fel.cvut.cz>
2021-04-29 09:20:26 +0200

    Added basic ghost movement

--- ghost.c
+++ ghost.c
@@ -12,13 +12,85 @@
 #include "ghost.h"
 #include "data_structures.h"
 #include <stdbool.h>
-//internal functions
-bool can_move(ghost_type *ghost, int dirx, int diry, map_data *map);
-// end of internal functions
+#include <stdlib.h>
 
+//internal functions and structure
+//returns true if pacman can move in given direction
+bool can_move(ghost_type *ghost, int dirx, int diry, map_data *map);
+
+//returns ammount of directions added to moves_arr
+int create_moves(moves_costs_t *moves_arr, ghost_type *ghost, map_data *map, pacman_type *pacman);
+
+typedef struct
+{
+    coords dir;
+    int cost;
+} moves_costs_t;
+// end of internal functions and structure
+
+void move(ghost_type *ghost, map_data *map, pacman_type *pacman)
+{
+    moves_costs_t possible_moves[4];
+    int possibilities = create_moves(possible_moves, ghost, map, pacman);
+    if (possibilities == 0)
+    {
+        //host reached dead_end
+        ghost->direction.x = -ghost->direction.x;
+        ghost->direction.x = -ghost->direction.y;
+    }
+    else if (possibilities == 1)
+    {
+        //only one way possible, pick that one
+        ghost->direction = possible_moves[0].dir;
+    }
+    else if (ghost->moving_randomly)
+    {
+        ghost->direction = possible_moves[rand() % possibilities].dir;
+    }
+    else
+    {
+        //pick direction with smallest cost
+        int lowest_cost = possible_moves[0].cost;
+        int lowest_index = 0;
+        for (int i = 1; i < possibilities; ++i)
+        {
+            if (lowest_cost > possible_moves[i].cost)
+            {
+                int lowest_cost = possible_moves[i].cost;
+                int lowest_index = i;
+            }
+        }
+        ghost->direction = possible_moves[lowest_index].dir;
+    }
+}
 
 bool can_move(ghost_type *ghost, int dirx, int diry, map_data *map)
 {
     int pixel = map->board_arr[map->width * (ghost->location.y + diry) + ghost->location.x + dirx];
     return (pixel != BLOCKED);
+}
+
+int create_moves(moves_costs_t *moves_arr, ghost_type *ghost, map_data *map, pacman_type *pacman)
+{
+    //both 4 on next lines is ammount of directions and should not be changed
+    coords dirs[4] = {{.x = 0, .y = -1}, {.x = 0, .y = 1}, {.x = 1, .y = 0}, {.x = -1, .y = 0}};
+    int ret = 0;
+    for (int i = 0; i < 4; ++i)
+    {
+        if (can_move(ghost, dirs[i].x, dirs[i].y, map))
+        {
+            if ((dirs[i].x != -ghost->direction.x) || (dirs[i].y != -ghost->direction.y))
+            {
+                //add the direction
+                moves_arr[ret].dir = dirs[i];
+                int cost = (pacman->location.x - ghost->location.x - dirs[i].x) *
+                               (pacman->location.x - ghost->location.x - dirs[i].x) +
+                           (pacman->location.y - ghost->location.y - dirs[i].y) *
+                               (pacman->location.y - ghost->location.y - dirs[i].y);
+                moves_arr[ret].cost = cost;
+                ret++;
+            }
+        }
+    }
+    return ret; //return ammount of valid options to move
 }
