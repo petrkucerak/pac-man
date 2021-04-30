9806a9e23c3928fe001b24c0b3d7a82825581322
Petr Kucera <petr@khome.cz>
2021-04-30 13:24:06 +0200

    create description and include import files

---
+++ final_score.c
@@ -0,0 +1,20 @@
+/*******************************************************************
+  Program to draw text to frame buffer on MicroZed
+  based MZ_APO board designed by Petr Porazil at PiKRON
+
+   final_score.h      - functions for draw final score
+
+  (C) Copyright 2021 by Petr Kucera
+      e-mail:   kucerp28@fel.cvut.cz
+      license:  any combination of GPL, LGPL, MPL or BSD licenses
+
+ *******************************************************************/
+
+#include <stdio.h>
+#include "final_score.h"
+#include "data_structures.h"
+#include "draw_shapes.h"
+#include "update_peripherals.h"
+#include "text_fb.h"
+#include <unistd.h>
+
