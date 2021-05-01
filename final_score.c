8be51a68de2e22472f7365a5362bde86dffd20ac
Petr Kucera <petr@khome.cz>
2021-05-01 20:26:22 +0200

    function draw final score

--- final_score.c
+++ final_score.c
@@ -18,3 +18,23 @@
 #include "text_fb.h"
 #include <unistd.h>
 
+void draw_final_score(int score, fb_data *frame_buff, unsigned char *lcd_mem_base, font_descriptor_t *font)
+{
+   char c = ' ';
+   while (c != 's' && c != 'q')
+   {
+      set_background(frame_buff, 0);
+      draw_text_center(frame_buff, "SKORE", frame_buff->width / 2, frame_buff->height / 10, 3, font, 0xffff);
+      char string_tmp[20];
+      snprintf(string_tmp, 20, "%d", score);
+      draw_text_center(frame_buff, string_tmp, frame_buff->width / 2, frame_buff->height / 2, 4, font, 0xffff);
+      draw_text_center(frame_buff, "SPUSTIT ZNOVU [s]", frame_buff->width / 2, frame_buff->height - frame_buff->height / 10, 2, font, 0xffff);
+      // update display
+      lcd_from_fb(frame_buff, lcd_mem_base);
+      // scan input
+      pthread_mutex_lock(&mtx);
+      pthread_cond_wait(&character_has_been_read, &mtx);
+      c = read_thread_data.last_read;
+      pthread_mutex_unlock(&mtx);
+   }
+}
