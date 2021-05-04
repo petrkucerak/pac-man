c80767f786a91d1b5ad4b6609e062e74d478ff1e
Petr Kucera <petr@khome.cz>
2021-05-04 22:13:06 +0200

    add doxygen comments

--- apoman.c
+++ apoman.c
@@ -1,14 +1,13 @@
-/*******************************************************************
-  Main function for project apoman for MicroZed based MZ_APO board
-  designed by Petr Porazil at PiKRON
-
-  apoman.c      - main file
-
-(C) Copyright 2021 by Petr Kucera and Lukas Nejezchleb
-      e-mails:   kucerp28@fel.cvut.cz , nejezluk@fel.cvut.cz
-      license:  any combination of GPL, LGPL, MPL or BSD licenses
- *******************************************************************/
-
+/**
+ * @file apoman.c
+ * @author Lukas Nejezchleb (nejezluk@fel.cvut.cz), Petr Kucera (kucerp28@fel.cvut.cz)
+ * @brief Module housing peripherals initialisation, necessary dynamic allocation of memory. Creates thread listening to the terminal input. Calls run init game menu().
+ * @version 0.1
+ * @date 2021-05-04
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #define _POSIX_C_SOURCE 200112L
 
 #include <stdlib.h>
@@ -44,7 +43,7 @@ pthread_cond_t character_has_been_read;
 read_thread_data_type read_thread_data;
 
 /**
- * @brief The main program fucntion
+ * @brief The main program function
  * 
  * @param argc the count of arguments
  * @param argv program arguments
